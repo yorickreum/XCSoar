38dc35b82383a50b200e26fab832179f38f81291
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-09-09 08:06:44 +0000

    time/PosixTimeZone: Add a parser for POSIX TZ strings

--- build/libtime.mk
+++ build/libtime.mk
@@ -9,6 +9,7 @@ TIME_SOURCES = \
 	$(TIME_SRC_DIR)/WrapClock.cpp \
 	$(TIME_SRC_DIR)/LocalTime.cpp \
 	$(TIME_SRC_DIR)/SystemTimeZone.cpp \
+	$(TIME_SRC_DIR)/PosixTimeZone.cpp \
 	$(TIME_SRC_DIR)/BrokenTime.cpp \
 	$(TIME_SRC_DIR)/BrokenDate.cpp \
 	$(TIME_SRC_DIR)/BrokenDateTime.cpp
